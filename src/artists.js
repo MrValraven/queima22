2d7edb3ebaaa7cb96ee5492b091e47fac02e7542
mrvalraven <akatiago@gmail.com>
2022-06-01 15:36:13 +0100

    added artists to main page

--- src/artists.js
+++ src/artists.js
@@ -2,12 +2,32 @@ import AnaMalhoaImage from "./assets/anamalhoa.jpg";
 import ProfjamImage from "./assets/profjam.jpg";
 import LonerImage from "./assets/loner.jpg";
 import TelioImage from "./assets/telio.jpg";
+import ChicoImage from "./assets/chico.png";
+import IvandroImage from "./assets/ivandro.png";
+import QuimImage from "./assets/quim.png";
+import BispoImage from "./assets/bispo.png";
+import PlutonioImage from "./assets/plutonio.png";
 
 const images = [
   {
     img: TelioImage,
     name: "Deejay Télio",
     date: "3 de Junho",
+  },
+  {
+    img: IvandroImage,
+    name: "Ivandro",
+    date: "3 de Junho",
+  },
+  {
+    img: ChicoImage,
+    name: "Chico da Tina",
+    date: "4 de Junho",
+  },
+  {
+    img: QuimImage,
+    name: "Quim Barreiros",
+    date: "6 de Junho",
   },
   {
     img: LonerImage,
@@ -18,6 +38,16 @@ const images = [
     img: ProfjamImage,
     name: "Profjam",
     date: "9 de Junho",
+  },
+  {
+    img: BispoImage,
+    name: "Bispo",
+    date: "10 de Junho",
+  },
+  {
+    img: PlutonioImage,
+    name: "Plutonio",
+    date: "10 de Junho",
   },
   {
     img: AnaMalhoaImage,
--- src/pages/Precario/Precario.jsx
+++ src/pages/Precario/Precario.jsx
@@ -37,6 +37,272 @@ const Precario = () => {
           <p className="preco">70€</p>
         </div>
       </div>
+      <h1>
+        Precário Bilhetes Diários
+        <br /> (Até &#62;&#62; 3 Junho 18h)
+      </h1>
+      <div className="container">
+        <h2>SÓCIO / BOLSEIRO</h2>
+        <div>
+          <p className="data">3 JUNHO</p>
+          <p className="preco">10€</p>
+        </div>
+        <div>
+          <p className="data">4 JUNHO</p>
+          <p className="preco">12€</p>
+        </div>
+        <div>
+          <p className="data">5 JUNHO</p>
+          <p className="preco">4€</p>
+        </div>
+        <div>
+          <p className="data">6 JUNHO</p>
+          <p className="preco">6€</p>
+        </div>
+        <div>
+          <p className="data">7 JUNHO</p>
+          <p className="preco">ENTRADA GRATUITA</p>
+        </div>
+        <div>
+          <p className="data">8 JUNHO</p>
+          <p className="preco">6€</p>
+        </div>
+        <div>
+          <p className="data">9 JUNHO</p>
+          <p className="preco">10€</p>
+        </div>
+        <div>
+          <p className="data">10 JUNHO</p>
+          <p className="preco">12€</p>
+        </div>
+        <div>
+          <p className="data">11 JUNHO</p>
+          <p className="preco">6€</p>
+        </div>
+        <div>
+          <p className="data">12 JUNHO</p>
+          <p className="preco">ENTRADA GRATUITA</p>
+        </div>
+      </div>
+      <div className="container">
+        <h2>ESTUDANTES</h2>
+        <div>
+          <p className="data">3 JUNHO</p>
+          <p className="preco">12€</p>
+        </div>
+        <div>
+          <p className="data">4 JUNHO</p>
+          <p className="preco">14€</p>
+        </div>
+        <div>
+          <p className="data">5 JUNHO</p>
+          <p className="preco">5€</p>
+        </div>
+        <div>
+          <p className="data">6 JUNHO</p>
+          <p className="preco">8€</p>
+        </div>
+        <div>
+          <p className="data">7 JUNHO</p>
+          <p className="preco">ENTRADA GRATUITA</p>
+        </div>
+        <div>
+          <p className="data">8 JUNHO</p>
+          <p className="preco">8€</p>
+        </div>
+        <div>
+          <p className="data">9 JUNHO</p>
+          <p className="preco">12€</p>
+        </div>
+        <div>
+          <p className="data">10 JUNHO</p>
+          <p className="preco">14€</p>
+        </div>
+        <div>
+          <p className="data">11 JUNHO</p>
+          <p className="preco">8€</p>
+        </div>
+        <div>
+          <p className="data">12 JUNHO</p>
+          <p className="preco">ENTRADA GRATUITA</p>
+        </div>
+      </div>
+      <div className="container">
+        <h2>NÃO ESTUDANTE</h2>
+        <div>
+          <p className="data">3 JUNHO</p>
+          <p className="preco">15€</p>
+        </div>
+        <div>
+          <p className="data">4 JUNHO</p>
+          <p className="preco">16€</p>
+        </div>
+        <div>
+          <p className="data">5 JUNHO</p>
+          <p className="preco">5€</p>
+        </div>
+        <div>
+          <p className="data">6 JUNHO</p>
+          <p className="preco">9€</p>
+        </div>
+        <div>
+          <p className="data">7 JUNHO</p>
+          <p className="preco">2€</p>
+        </div>
+        <div>
+          <p className="data">8 JUNHO</p>
+          <p className="preco">9€</p>
+        </div>
+        <div>
+          <p className="data">9 JUNHO</p>
+          <p className="preco">14€</p>
+        </div>
+        <div>
+          <p className="data">10 JUNHO</p>
+          <p className="preco">16€</p>
+        </div>
+        <div>
+          <p className="data">11 JUNHO</p>
+          <p className="preco">10€</p>
+        </div>
+        <div>
+          <p className="data">12 JUNHO</p>
+          <p className="preco">3€</p>
+        </div>
+      </div>
+      <h1>
+        Precário Bilhetes Diários
+        <br /> (Após 18h de 3 Junho)
+      </h1>
+      <div className="container">
+        <h2>SÓCIO / BOLSEIRO</h2>
+        <div>
+          <p className="data">3 JUNHO</p>
+          <p className="preco">11€</p>
+        </div>
+        <div>
+          <p className="data">4 JUNHO</p>
+          <p className="preco">13€</p>
+        </div>
+        <div>
+          <p className="data">5 JUNHO</p>
+          <p className="preco">5€</p>
+        </div>
+        <div>
+          <p className="data">6 JUNHO</p>
+          <p className="preco">7€</p>
+        </div>
+        <div>
+          <p className="data">7 JUNHO</p>
+          <p className="preco">1€</p>
+        </div>
+        <div>
+          <p className="data">8 JUNHO</p>
+          <p className="preco">7€</p>
+        </div>
+        <div>
+          <p className="data">9 JUNHO</p>
+          <p className="preco">11€</p>
+        </div>
+        <div>
+          <p className="data">10 JUNHO</p>
+          <p className="preco">13€</p>
+        </div>
+        <div>
+          <p className="data">11 JUNHO</p>
+          <p className="preco">7€</p>
+        </div>
+        <div>
+          <p className="data">12 JUNHO</p>
+          <p className="preco">1€</p>
+        </div>
+      </div>
+      <div className="container">
+        <h2>ESTUDANTES</h2>
+        <div>
+          <p className="data">3 JUNHO</p>
+          <p className="preco">13€</p>
+        </div>
+        <div>
+          <p className="data">4 JUNHO</p>
+          <p className="preco">15€</p>
+        </div>
+        <div>
+          <p className="data">5 JUNHO</p>
+          <p className="preco">6€</p>
+        </div>
+        <div>
+          <p className="data">6 JUNHO</p>
+          <p className="preco">9€</p>
+        </div>
+        <div>
+          <p className="data">7 JUNHO</p>
+          <p className="preco">1€</p>
+        </div>
+        <div>
+          <p className="data">8 JUNHO</p>
+          <p className="preco">9€</p>
+        </div>
+        <div>
+          <p className="data">9 JUNHO</p>
+          <p className="preco">13€</p>
+        </div>
+        <div>
+          <p className="data">10 JUNHO</p>
+          <p className="preco">15€</p>
+        </div>
+        <div>
+          <p className="data">11 JUNHO</p>
+          <p className="preco">9€</p>
+        </div>
+        <div>
+          <p className="data">12 JUNHO</p>
+          <p className="preco">1€</p>
+        </div>
+      </div>
+      <div className="container">
+        <h2>NÃO ESTUDANTE</h2>
+        <div>
+          <p className="data">3 JUNHO</p>
+          <p className="preco">16€</p>
+        </div>
+        <div>
+          <p className="data">4 JUNHO</p>
+          <p className="preco">17€</p>
+        </div>
+        <div>
+          <p className="data">5 JUNHO</p>
+          <p className="preco">6€</p>
+        </div>
+        <div>
+          <p className="data">6 JUNHO</p>
+          <p className="preco">10€</p>
+        </div>
+        <div>
+          <p className="data">7 JUNHO</p>
+          <p className="preco">3€</p>
+        </div>
+        <div>
+          <p className="data">8 JUNHO</p>
+          <p className="preco">10€</p>
+        </div>
+        <div>
+          <p className="data">9 JUNHO</p>
+          <p className="preco">15€</p>
+        </div>
+        <div>
+          <p className="data">10 JUNHO</p>
+          <p className="preco">17€</p>
+        </div>
+        <div>
+          <p className="data">11 JUNHO</p>
+          <p className="preco">11€</p>
+        </div>
+        <div>
+          <p className="data">12 JUNHO</p>
+          <p className="preco">4€</p>
+        </div>
+      </div>
     </div>
   );
 };
